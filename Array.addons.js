baf421f044170a555d677c6e1173fe5b697a92e8
Frederik Leidloff <fleidloff@team.mobile.de>
2016-04-14 12:37:39 +0200

    move Array addons to its own file

---
+++ Array.addons.js
@@ -0,0 +1,19 @@
+Array.prototype.isEmpty = function() {
+    return this.length === 0;
+};
+
+Array.prototype.head = function() {
+    return this[0] || null;
+};
+
+Array.prototype.tail = function() {
+    return this.length > 1 ? this.slice(1) : [];
+};
+
+Array.prototype.left = function() {
+    return this.slice(0, this.length / 2);
+};
+
+Array.prototype.right = function() {
+    return this.slice(this.length / 2, this.length);
+};
--- mergesort/index.js
+++ mergesort/index.js
@@ -1,41 +1,23 @@
-Array.prototype.isEmpty = function() {
-    return this.length === 0;
-};
-
-Array.prototype.head = function() {
-    return this[0] || null;
-};
-
-Array.prototype.tail = function() {
-    return this.length > 1 ? this.slice(1) : [];
-}; 
-
-Array.prototype.left = function() {
-    return this.slice(0, this.length / 2);
-};
-
-Array.prototype.right = function() {
-    return this.slice(this.length / 2, this.length);
-};
+require("../Array.addons.js");
 
 const list = [6, 4, 7, 9, 11, -4, 5, 2, 5, 90];
-
-function mergeSort(l) {
-    if (l.tail().isEmpty()) {
-        return l;
-    }
-    return merge(mergeSort(l.left()), mergeSort(l.right()));
-}
-
-function merge(l, r, result = []) {
-    switch(true) {
-        case l.isEmpty() || r.isEmpty():
-            return result.concat(l).concat(r);
-        case l.head() <= r.head():
-            return merge(l.tail(), r, result.concat(l.head()));
-        default:
-            return merge(l, r.tail(), result.concat(r.head()));
-    }
-}
-
 console.log(mergeSort(list));
+
+function mergeSort(arr) {
+    if (arr.tail().isEmpty()) {
+        return arr;
+    }
+    return merge(mergeSort(arr.left()), mergeSort(arr.right()));
+}
+
+function merge(left, right, result = []) {
+    switch(true) {
+        case left.isEmpty() || right.isEmpty():
+            return result.concat(left).concat(right);
+        case left.head() <= right.head():
+            return merge(left.tail(), right, result.concat(left.head()));
+        default:
+            return merge(left, right.tail(), result.concat(right.head()));
+    }
+}
+
