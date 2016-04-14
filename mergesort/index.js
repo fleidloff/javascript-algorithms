53766967c8dbf4b70a271ff8990a270f104fec5e
Frederik Leidloff <fleidloff@team.mobile.de>
2016-04-14 11:38:40 +0200

    implement mergesort

--- mergesort/index.js
+++ mergesort/index.js
@@ -3,14 +3,53 @@ Array.prototype.isEmpty = function() {
 };
 
 Array.prototype.head = function() {
-    return this[0] || false;
+    return this[0] || null;
 };
 
 Array.prototype.tail = function() {
-    return this.length > 1 ? this.slice(1) : false;
+    return this.length > 1 ? this.slice(1) : [];
 }; 
 
-const a = [1, 2, 3];
-const tail = a.tail().tail();
+Array.prototype.left = function() {
+    return this.slice(0, this.length / 2);
+};
 
-console.log(true === !!tail, tail);
+Array.prototype.right = function() {
+    return this.slice(this.length / 2, this.length);
+};
+
+const list = [6, 4, 7, 9, 11, -4, 5, 2, 5, 90];
+
+function mergeSort(l) {
+    if (l.tail().isEmpty()) {
+        return l;
+    }
+    return merge(mergeSort(l.left()), mergeSort(l.right()));
+}
+
+function merge(l, r) {
+    const result = [];
+    while(!l.isEmpty() && !r.isEmpty()) {
+        if (l.head() <= r.head()) {
+            result.push(l.head());
+            l = l.tail();
+        } else {
+            result.push(r.head());
+            r = r.tail();
+        }  
+    }
+    
+    while(!l.isEmpty()) {
+        result.push(l.head());
+        l = l.tail();
+    }
+
+    while(!r.isEmpty()) {
+        result.push(r.head());
+        r = r.tail();
+    }
+    
+    return result;
+}
+
+console.log(mergeSort(list));
