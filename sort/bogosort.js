402dba8290edf23726fe7e8af94bda2cadd18a39
Frederik Leidloff <fleidloff@team.mobile.de>
2016-04-19 08:58:21 +0200

    use for loop for bogo sort

--- sort/bogosort.js
+++ sort/bogosort.js
@@ -6,9 +6,7 @@ require("../Array.addons.js");
 console.log(bogosort([6, 4, 7, 9, -2, 7, 90, 5, 2]));
 
 function bogosort(items) {
-    while(!items.isSorted()) {
-        items.shuffle();
-    }
+    for (;!items.isSorted(); items.shuffle()) {}
 
     return items;
 }
