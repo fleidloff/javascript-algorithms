5cf04faae5e010c9da7bfea0036a2334e165776b
Frederik Leidloff <fleidloff@team.mobile.de>
2016-11-10 13:35:21 +0100

    add todo

--- run.sh
+++ run.sh
@@ -8,3 +8,4 @@ echo "bogo"; node --harmony sort/bogosort.js // --harmony required because of ta
 
 // TODO:
     * use immutable.js https://facebook.github.io/immutable-js/docs/#/List
+    * or lodash https://github.com/lodash/lodash/wiki/FP-Guide
