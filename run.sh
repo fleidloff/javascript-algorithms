1fcb42f9d67cd6a6850be3c8b1df047f6011d408
Frederik Leidloff <kontakt@frederik-leidloff.de>
2016-11-11 21:06:09 +0100

    immutable mergesort

--- run.sh
+++ run.sh
@@ -6,8 +6,8 @@ echo "selection"; node sort/selectionsort.js
 
 echo "bogo"; node --harmony sort/bogosort.js // --harmony required because of tail call recursion
 
-// TODO:
-    * use immutable.js https://facebook.github.io/immutable-js/docs/#/List
-    * or lodash https://github.com/lodash/lodash/wiki/FP-Guide
-    * add Optional stuff (to enable a.b.c without errors even when a.b is undefined)
-    * eslint
+# TODO: 
+#    * use immutable.js https://facebook.github.io/immutable-js/docs/#/List
+#    * or lodash https://github.com/lodash/lodash/wiki/FP-Guide
+#    * add Optional stuff (to enable a.b.c without errors even when a.b is undefined)
+#    * eslint
--- sort/mergesort.js
+++ sort/mergesort.js
@@ -1,16 +1,16 @@
-require("../Array.addons.js");
 const Y = require("../Y.js");
+const I = require("immutable");
 
 const mergeSort = Y((items) =>
-    items.tail().isEmpty() ?
+    items.rest().isEmpty() ?
         items 
-    :   merge(mergeSort(items.left()), mergeSort(items.right())));
+    :   merge(mergeSort(items.take(items.count()/2)), mergeSort(items.skip(items.count()/2))));
 
-const merge = Y((left, right, result = []) =>
+const merge = Y((left, right, result = new I.List()) => 
     left.isEmpty() || right.isEmpty() ?
         result.concat(left).concat(right) :
-    left.head() <= right.head() ?  
-        merge(left.tail(), right, result.concat(left.head())) 
-    :   merge(left, right.tail(), result.concat(right.head())));
+    left.first() <= right.first() ?  
+        merge(left.rest(), right, result.concat(left.first())) 
+    :   merge(left, right.rest(), result.concat(right.first())));
 
-console.log(mergeSort([6, 4, 7, 9, 11, -4, 5, 2, 5, 90]));
+console.log(mergeSort(I.fromJS([6, 4, 7, 9, 11, -4, 5, 2, 5, 90])));
