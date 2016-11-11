4bf5d02c5550531f08292195f8ee65644740505b
Frederik Leidloff <kontakt@frederik-leidloff.de>
2016-11-11 21:31:35 +0100

    immutable bogo sort

--- sort/bogosort.js
+++ sort/bogosort.js
@@ -1,11 +1,22 @@
 // node --harmony sort/bogosort.js
 "use strict";
-require("../Array.addons.js");
 const Y = require("../Y.js");
+const I = require("immutable");
 
 const bogoSort = Y((items) =>
-	items.isSorted() ?
+	isSorted(items) ?
         items
-    :   bogoSort(items.shuffle()));
+    :   bogoSort(shuffle(items)));
 
-console.log(bogoSort([6, 4, 7, 9, -2, 7, 90, 5, 2]));
+const isSorted = items =>
+	items.sort().equals(items);
+
+const shuffle = items =>
+	items.reduce((memo, item, idx) => {
+		const rnd = Math.floor(Math.random() * items.count());
+		return memo
+			.set(idx, memo.get(rnd))
+			.set(rnd, memo.get(idx))
+	}, items);
+
+console.log(bogoSort(I.fromJS([6, 4, 7, 9, -2, 7, 90, 5, 2])));
