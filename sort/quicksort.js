a2856f323297a5071836b6fa0c9f0bddf9884a50
Frederik Leidloff <kontakt@frederik-leidloff.de>
2016-11-11 21:08:09 +0100

    immutable quicksort

--- sort/quicksort.js
+++ sort/quicksort.js
@@ -1,12 +1,13 @@
 require("../Array.addons.js");
 const Y = require("../Y.js");
+const I = require("immutable");
 
-const quickSort = Y((items, pivot = items.head()) =>
-    items.tail().isEmpty() ?
+const quickSort = Y((items, pivot = items.first()) =>
+    items.rest().isEmpty() ?
         items
-    : 	combine(items.tail().filterLeftRight(it => it < pivot), pivot));
+    : 	combine(items.rest().filter(it => it < pivot), items.rest().filter(it => !(it < pivot)), pivot));
 
-const combine = ({ left, right }, pivot) => 
+const combine = ( left, right , pivot) => 
     quickSort(left).concat(pivot).concat(quickSort(right));
 
-console.log(quickSort([6, 4, 7, 9, 11, -4, 5, 2, 5, 90]));
+console.log(quickSort(I.fromJS([6, 4, 7, 9, 11, -4, 5, 2, 5, 90])));
