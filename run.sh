c681c4d97e650f5c337aece4b75adf155e943715
Frederik Leidloff <fleidloff@team.mobile.de>
2016-11-14 10:36:37 +0100

    class properties

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 echo "bubble"; node sort/bubblesort.js
-echo "insertion"; node sort/insertionsort.js
+echo "insertion"; babel-node sort/insertionsort.js
 echo "merge"; node sort/mergesort.js
 echo "quick"; node sort/quicksort.js
 echo "selection"; node sort/selectionsort.js
--- sort/insertionsort.js
+++ sort/insertionsort.js
@@ -3,18 +3,19 @@ const { List } = require("extendable-immutable");
 
 class InsertionSort extends List {
 
-    sort() {
-        return this.reduce((memo, item) =>
+    sort = () =>
+         this.reduce((memo, item) =>
             memo.insert(memo.findIndex((r) => r >= item), item)
         , new List());
-    }
 
-    findIndex(condition) {
-        const idx = super.findIndex(condition);
+    findIndex = (condition) => {
+        const idx = this.super.findIndex(condition)
+
         return idx < 0 ?
             this.count() :
             idx;
     }
+
 }
 
 console.log(new InsertionSort([6, 4, 7, 9, 11, -4, 5, 2, 5, 90]).sort());
