206f5a729ecf682b54a3a1c0cb9aa606748c2705
Frederik Leidloff <fleidloff@team.mobile.de>
2016-11-15 08:22:20 +0100

    better compareToBool function

--- sort/insertionsort.js
+++ sort/insertionsort.js
@@ -3,8 +3,8 @@ const I = require("immutable");
 const InsertionSort = (list, compare = defaultCompare) =>
     list.reduce((memo, item) =>
         memo.insert(
-            findPositiveIndex(memo, (it) => compareToBool(compare(item, it))),
-            item,
+            findPositiveIndex(memo, compareToBool(compare, item)),
+            item
         )
     , new I.List());
 
@@ -17,10 +17,14 @@ const findPositiveIndex = (list, condition) => {
         idx;
 };
 
-const compareToBool = (compareResult) =>
-    typeof compareResult === "boolean" ?
+const compareToBool = (compare, item) => (it) => {
+    const compareResult = compare(item, it);
+
+    return typeof compareResult === "boolean" ?
         compareResult :
-        compareResult === -1;
+        compareResult === -1
+};
+
 
 console.log(InsertionSort([6, 4, 7, 9, 11, -4, 5, 2, 5, 90]));
 console.log(InsertionSort("Frederik".split(""), (a, b) => a.toLowerCase().localeCompare(b.toLowerCase())));
