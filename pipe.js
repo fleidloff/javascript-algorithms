83d8526dc97fd1ef35658e05ed67b384ce06f937
fleidloff <kontakt@frederik-leidloff.de>
2016-12-20 14:09:01 +0100

    Create pipe.js

---
+++ pipe.js
@@ -0,0 +1,23 @@
+function shuffle(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    return array;
+}
+
+const a = [1, 2, 3];
+
+// console.log(JSON.parse(JSON.stringify(shuffle(a))));
+const pipe = (...funs) => (input) => funs.reduce((memo, fun) => typeof fun === "function" ? fun(memo) : fun);
+
+const res = pipe(
+  a,
+  shuffle, 
+  JSON.stringify, 
+  JSON.parse
+)();
+
+console.log(res);
